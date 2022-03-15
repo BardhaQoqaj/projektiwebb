e08aca6a6913e734be2a71defac81c5e62fea3b1
bardha cocaj <fc52610@ubt-uni.net>
2022-03-15 13:53:27 +0100

    imgs

---
+++ projektiwebF3/img/index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<body>
+    <?php include 'header.php' ?>
+    <div class="about">
+        <div class="inner-section">
+            <h1>Rreth nesh</h1>
+            <p class="text">
+                 Healthy Lives Online është një furnizues i ushqimit shëndetësor, me qendër në Ulpiane, Imzot Nik Prela Prishtine.
+                 Ne ofrojmë ushqime të plota natyrale dhe organike me cilësi të lartë,
+                 si dhe produkte të lidhura me jetesën e shëndetshme, drejtpërdrejt në shtëpitë dhe bizneset në të gjithë MB dhe Evropë.
+                 Në zemër, Healthy Lives ka qenë gjithmonë një biznes i drejtuar nga familja. Një dashuri e përbashkët për ushqimet e plota bëri që kushërinjtë
+                 Arthur Martin dhe Joe Cooper të krijonin kompaninë në vitin 2007 në Francë. Përveç përvojës së tyre mjaft të rëndësishme në industri, Arthur dhe
+                   Joe filluan me një minimum burimesh (dhe një sasi të madhe pasioni),. Kompania u rrit jashtëzakonisht shpejt 
+                   nga viti në vit, duke u zhvilluar në kompaninë gjithnjë e më të suksesshme të produkteve ushqimore të plota që është sot.. 
+                   <br>Joe Cooper ka përvojë dhe ekspertizë të gjerë si në marketing ashtu edhe në dizajn në platforma të ndryshme. Fokusi i tij
+                    kryesor janë faqja e internetit, përvoja e përdoruesit, dizajni, aktivitetet e marketingut dhe të gjitha ndërveprimet midis kompanisë dhe klientëve tanë 
+                    të shumtë evropianë.
+                   Arthur Martin ka mbi 25 vjet përvojë në industrinë e ushqimeve të plota. Ai është gjithmonë në vend si menaxher i magazinës, duke u marrë me gjithçka, 
+                   nga furnizimi dhe përzgjedhja e produkteve, te kontrolli i cilësisë, koordinimi i stafit dhe të gjitha operacionet e magazinës. </br>
+            </p>
+            <div class="skills">
+                <button><a  class="butoni" href="http://localhost:8081/Projekti%20Web/contact%20Us/contactUs.php">Contact Us</a></button>
+            </div>
+           
+        </div>
+       
+    </div>
+   <?php include 'footer.php' ?>
+</body>
+</html>
