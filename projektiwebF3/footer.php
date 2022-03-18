26205c2c852e15bc6c4f1eb48a0cd376df8759f9
endihaziri <eh51545@ubt-uni.net>
2022-03-18 16:00:33 +0100

    footeri

---
+++ projektiwebF3/footer.php
@@ -0,0 +1,4 @@
+<section class="footer">
+    <p>123 Rruga 'Imzot Nik Prela', Prishtine , Kosove | Phone: 044-391-447,045-364-798 | Email: HealthyLives@gmail.com</p>
+    <p> Copyright © 2021 foodsitemadebyus</p>
+</section>
