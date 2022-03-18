1da9974240bdeca4063acf98e45f0648875f77ef
bardha cocaj <fc52610@ubt-uni.net>
2022-03-18 16:17:02 +0100

    pa pershkrim

---
+++ projektiwebF3/products.php
@@ -0,0 +1,243 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Healthy Lives Products</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./menu/style.css">
+</head>
+<body>
+<div class="menu">
+        <div class="heading">
+            <h1>Healthy Lives food items</h1>
+            <h3>&mdash; OUR PRODUCTS &mdash;</h3>
+            <h4> If you want to order please login </h4>
+            <div class="nav">
+<?php include 'header.php' ?>
+</div>
+</div>
+        <div class="food-items">
+           <img src="./img/chicken chill.jpg" alt="Chicken Chill Hamburger">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Chicken Chill Hamburger</h5>
+                    <h5 class="price"> 3.20€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/gorgeous gang.jpg" alt="Gorgeous Gang Hamburger">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Gorgeous Gang Hamburger</h5>
+                    <h5 class="price"> 2.90€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/salmon taste.jpg" alt="Salmon Taste Hamburger">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Salmon Taste Hamburger</h5>
+                    <h5 class="price"> 4.20€ </h5>
+                    <p> </p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/queen burger.jpeg" alt="Queen Hamburger">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Queen Hamburger</h5>
+                    <h5 class="price"> 3.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/tuna turner.jpg" alt="Tuna Turner Wrap">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Tuna Turner Wrap</h5>
+                    <h5 class="price"> 3.20€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/Chicken Charm.jpg" alt="Chicken Charm Wrap">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Chicken Charm Wrap</h5>
+                    <h5 class="price"> 3.90€ </h5>
+                    <p> </p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/cool egg.jpg" alt="Cool Egg Wrap">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Cool Egg Wrap</h5>
+                    <h5 class="price"> 4.20€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/avocado egg.jpg" alt="Avocado&Egg Wrap">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Avocado & Egg Wrap</h5>
+                    <h5 class="price"> 3.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/positive calori.png" alt="Positive Calorie Salad">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Positive Calorie Salad</h5>
+                    <h5 class="price"> 3.90€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/healing power.jpg" alt="Healing Power Salad">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Healing Power Salad</h5>
+                    <h5 class="price"> 4.50€ </h5>
+                    <p> </p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/vegie love.jpg" alt="Veggie Love Salad">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Veggie Love Salad</h5>
+                    <h5 class="price"> 4.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/sweet buddha.jpg" alt="Sweet Buddha Salad">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Sweet Buddha Salad</h5>
+                    <h5 class="price"> 3.50€ </h5>
+                    <p> </p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/antioxidant smoothie.jpg" alt="Antioxidant Hero">
+            <div class="details">
+                <div class="details-sub">
+                    <h5>Antioxidant Hero </h5>
+                    <h5 class="price"> 2.90€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/whey protein smoothie.jpg" alt="Whey Protein &PB Smoothie">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Whey Protein &PB Smoothie</h5>
+                    <h5 class="price"> 3.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/orange smoothie.jpg" alt="Happy Oranges Smoothie ">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Happy Oranges Smoothie </h5>
+                    <h5 class="price"> 2.90€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/ginger lover smoothie.jpg" alt="Ginger Lover Smoothie">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Ginger Lover Smoothie</h5>
+                    <h5 class="price"> 2.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/peanut butter smoothie.jpg" alt="Peanut Butter Pudding">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Peanut Butter Pudding</h5>
+                    <h5 class="price"> 3.80€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/fruits and granolla.jpg" alt="Fruits & Granolla">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Fruits & Granolla</h5>
+                    <h5 class="price"> 3.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/choclate chia pudding.jpg" alt="Choclate Chia Pudding">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Choco Lover Chia Pudding</h5>
+                    <h5 class="price"> 3.90€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="food-items">
+            <img src="./img/pink vibes.jpg" alt="Pink Vibes Pudding">
+            <div class="details">
+                <div class="details-sub">
+                    <h5> Pink Vibes Pudding</h5>
+                    <h5 class="price"> 3.50€ </h5>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'footer.php' ?>
+</body>
+</html>
