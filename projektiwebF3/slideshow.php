60a818f36ead5d0a18642e2a828dc0f1b1f6641e
bardha cocaj <fc52610@ubt-uni.net>
2022-03-28 16:07:35 +0200

    slider

---
+++ projektiwebF3/slideshow.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Slideshow</title>
+	<link rel="stylesheet" type="text/css" href="./slider2/img.css">
+</head>
+<body>
+		<header>
+		<?php include 'header.php' ?>
+	<div class="slider">
+		<h1>Me dashuri nga duart tona , ne tryezat tuaja</h1>
+		<p>Ju jeni ajo që hani . Ju keni një zgjedhje në atë që jeni bërë dhe që doni të bëheni.</p>
+		<div class="slider-content">
+			<img src="./img/foto2.jpg" >
+		</div>
+		<div class="slider-content">
+			<img src="./img/foto3.jpg">
+		</div>
+        <div class="slider-content">
+			<img src="./img/foto4.jpg">
+
+		</div>
+
+		<button class="btn" onclick="prev()">Prev</button>
+			<button class="btn" onclick="next()">Next</button>
+			
+	</div>
+</header>
+	<div class="slider1-content">
+		<h1>Ndjehuni te sigurte!</h1>
+		   <p>Arsimi është thelbësor për vizionin tonë dhe është faktori më i rëndësishëm për të ndihmuar në përfundimin e pasigurisë ushqimore dhe humbjes së ushqimit në të njëjtën kohë. Ne kemi një nutricionist në shtëpi, një kuzhinier profesionist dhe staf arsimor për të siguruar që ne ofrojmë burimet më të mira të mundshme!
+			    <br></br> Qëllimi ynë është t'ju ofrojmë ushqime të freskëta, të shëndetshme dhe me vlera ushqyese. Ju lutemi shqyrtoni informacionet në çdo pjesë të menysë nëse keni alergji, ndjeshmëri ose preferenca të ndryshme ushqimore.
+			Ndjehuni të lirë të komunikoni me stafin tonë per informacione të detajizuara mbi produktet që mund të iu shkaktojnë alergji<br>Ne jemi krenar që e servojmë ushqimin të freskët, dhe në mënyrën më të përsosur — e përgatitur me kualitet, dhe dashuri
+			nga  punëtorët tanë në kuzhinë.</br>Ushqimi që hani mund të jetë ose forma më e sigurt dhe më e fuqishme e ilaçit ose forma më e ngadaltë e helmit, prandaj është  koha të vetëdisohemi dhe ne mund të ju vijmë në ndihmë. </p>
+	   </div>
+
+	   <?php include'footer.php' ?>
+	<script type="text/javascript" src="./js/javas1.js"></script>
+</body>
+</html>
